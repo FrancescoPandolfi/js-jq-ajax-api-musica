ef8a712f5713d2df80413590c649e8f398aaedb7
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-31 17:39:52 +0100

    Aggiunto filtro categorie

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ $.ajax({
   method: "GET",
   success: function (data, stato) {
 
-    var arrayResponse = data.response;
-    processData(arrayResponse);
+    var response = data.response;
+    processData(response);
 
   },
   error: function (richiesta, stato, errore) {
@@ -17,29 +17,71 @@ $.ajax({
   }
 });
 
+function processData(discList) {
+  discList.forEach(function (cd) {
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+    var context = cd;
+    var html = template(context);
+    $('.cds-container').append(html);
+  });
+}
+
+$("select").change(function(){
+  var selectVal = $(this).val();
+  console.log(selectVal);
+  $('.cds-container').text('');
+
+  $.ajax({
+    url: "https://flynn.boolean.careers/exercises/api/array/music",
+    method: "GET",
+    success: function (data, stato) {
+
+      var response = data.response;
+
+        response.forEach(function (cd) {
+          var source = $("#entry-template").html();
+          var template = Handlebars.compile(source);
+          var context = cd;
+          var html = template(context);
+
+          if (selectVal == cd.genre) {
+            $('.cds-container').append(html);
+          } else if (selectVal == 'Tutti') {
+            $('.cds-container').append(html);
+          }
 
 
-//
+
+        });
+
+
+    },
+    error: function (richiesta, stato, errore) {
+      alert("E' avvenuto un errore. " + errore);
+    }
+  });
+
+  // function processData(discList) {
+  //   discList.forEach(function (cd) {
+  //     var source = $("#entry-template").html();
+  //     var template = Handlebars.compile(source);
+  //     var context = cd;
+  //     var html = template(context);
+  //     $('.cds-container').append(html);
+  //   });
+  // }
 
 
 
-  function processData() {
-    i=0;
-    arrayResponse.forEach(function(item) {
-      // console.log(item);
-      var source = $("#entry-template").html();
-      var template = Handlebars.compile(source);
 
-      var context = arrayResponse[i];
-      var html = template(context);
-
-      $('.cds-container').append(html);
-      i++;
-
-    });
-  }
+});
 
 
+
+
+
+$(document)
 
 
 
